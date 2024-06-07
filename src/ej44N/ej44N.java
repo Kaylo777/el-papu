8082c3ef42ca8b6c46ea3493680faea88eff0e59
Kaylo777 <santinomoreno821@gmail.com>
2024-06-07 12:04:53 -0300

    tempanos

--- src/ej44N/ej44N.java
+++ src/ej44N/ej44N.java
@@ -1,10 +1,37 @@
 package ej44N;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Scanner;
 
 public class ej44N {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList<String> equipo = new ArrayList<>();
+		
+		  Scanner scanner = new Scanner(System.in);
+		  
+		  System.out.println("a ver los equipos que te sabes :u (escriba 'fin' para terminar TwT):");
+	        String entrada = scanner.nextLine();
+	        
+	        while (!entrada.equalsIgnoreCase("fin")) {
+	            equipo.add(entrada);
+	            entrada = scanner.nextLine();
+	        }
+	        HashSet<String> set = new HashSet<>(equipo);
 
+	        equipo.clear();
+
+	        equipo.addAll(set);
+
+		
+			Collections.sort(equipo);
+			
+			  int tamano = equipo.size();
+		        System.out.println("pusiste estos equios: "+tamano); 
+		        
+		        System.out.println("los equipos son: " + equipo);
+			
 	}
 
 }
